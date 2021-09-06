4eefcd42c38f644a1584e28718075031fcfaf86e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-06 22:55:34 +0800

    Delete flagitious.rb

--- flagitious.rb
+++
@@ -1,7 +0,0 @@
-eval =eval='eval(q=%q[puts"eval =eval=\47eval(q=%q[#{q}]);exit#\47##\47
-\" \#{eval eval if eval.size>210}}\".+i rescue##\"
-eval\"eval.scan(//){a=$`<<16700-eval.sum<<$\47;a.hash%2646==1447&&eval(a)}#\"##\"
-eval eval"]);exit#'##'
-" #{eval eval if eval.size>210}}".+i rescue##"
-eval"eval.scan(//){a=$`<<16700-eval.sum<<$';a.hash%2646==1447&&eval(a)}#"##"
-eval eval
